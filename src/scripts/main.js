7dbe7d36a8ec914539fa5bb3865a952a0dfafbf2
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-24 15:46:42 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const coordsWall = wall.getBoundingClientRect();
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const coordX = e.pageX - coordsWall.left - 25;
+  const coordY = e.pageY - coordsWall.top - 25;
+
+  if (coordX > coordsWall.height || coordX < 0) {
+    return;
+  }
+
+  if (coordY > coordsWall.width || coordY < 0) {
+    return;
+  }
+
+  spider.style.transform = `translate(${coordX}px, ${coordY}px)`;
 });
